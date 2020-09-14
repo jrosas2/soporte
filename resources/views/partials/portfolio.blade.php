a0cee83f66a52f9ea535f3007aac088e1b36f943
juan rosas <juan.rosas17@gmail.com>
2020-09-14 20:40:41 -0300

    cambio de galeria

--- resources/views/partials/portfolio.blade.php
+++ resources/views/partials/portfolio.blade.php
@@ -30,12 +30,6 @@
                 </a>
             </div>
 
-{{--         <div class="item brand col-sm-6 col-md-4 col-lg-4 col-xl-3 mb-4">
-<a href="assets/images/img_3.jpg" class="item-wrap fancybox" data-fancybox="gallery2">
-<span class="icon-search2"></span>
-<img class="img-fluid" src="assets/images/img_3.jpg">
-</a>
-</div> --}}
 
 <div class="item design col-sm-6 col-md-4 col-lg-4 col-xl-3 mb-4">
 
@@ -52,6 +46,12 @@
         <img class="img-fluid" src="assets/images/img_5.jpg">
     </a>
 </div>
+{{--         <div class="item brand col-sm-6 col-md-4 col-lg-4 col-xl-3 mb-4">
+<a href="assets/images/img_3.jpg" class="item-wrap fancybox" data-fancybox="gallery2">
+<span class="icon-search2"></span>
+<img class="img-fluid" src="assets/images/img_3.jpg">
+</a>
+</div> --}}
 
 {{--         <div class="item brand col-sm-6 col-md-4 col-lg-4 col-xl-3 mb-4">
 <a href="assets/images/img_6.jpg" class="item-wrap fancybox" data-fancybox="gallery2">
@@ -95,20 +95,130 @@
 </a>
 </div>
 
-<div class="item design col-sm-6 col-md-4 col-lg-4 col-xl-3 mb-4">
-    <a href="assets/images/img_12.jpg" class="item-wrap fancybox" data-fancybox="gallery2">
-        <span class="icon-search2"></span>
-        <img class="img-fluid" src="assets/images/img_12.jpg">
-    </a>
-</div>
-
-<div class="item web col-sm-6 col-md-4 col-lg-4 col-xl-3 mb-4">
-    <a href="assets/images/img_13.jpg" class="item-wrap fancybox" data-fancybox="gallery2">
-        <span class="icon-search2"></span>
-        <img class="img-fluid" src="assets/images/img_13.jpg">
-    </a>
-</div>
 
 </div>
 </div>
+</section>
+
+
+<!--==========================
+Porfolio Section
+============================-->
+
+
+
+<section id="portfolio">
+    <div class="container wow fadeInUp">
+
+
+        <div class="row item design">
+            <div class="col-md-12">
+                <h3 class="section-title text-center">Galería</h3>
+                <div class="section-title-divider"></div>
+                <p class="section-description">Ingesa a nuestra galería de imágenes</p>
+            </div>
+        </div>
+        <div class="row justify-content-center mb-5" data-aos="fade-up">
+            <div id="filters" class="filters text-center button-group col-md-7">
+                {{-- <button class="btn btn-primary active" data-filter="*">Todo</button> --}}
+                <button class="btn btn-primary" data-filter=".web">Sitios Web</button>
+                <button class="btn btn-primary" data-filter=".design">Reparaciones</button>
+                {{-- <button class="btn btn-primary" data-filter=".brand">Soporte</button> --}}
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-3 design">
+                <a class="portfolio-item fancybox design" data-fancybox="gallery2"  style="background-image: url(assets/images/img_1.jpg);" href="{{ asset('assets/images/img_1.jpg') }}">
+                    <div class="details">
+                        <h4>Arica</h4>
+                        <span>Empresa xxxxx</span>
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md-3 web">
+                <a class="portfolio-item fancybox " data-fancybox="gallery2" style="background-image: url(assets/images/img_2.jpg);" href="{{ asset('assets/images/img_2.jpg') }}">
+                    <div class="details">
+                        <h4>Portfolio 2</h4>
+                        <span>Alored dono par</span>
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md-3">
+                <a class="portfolio-item fancybox" data-fancybox="gallery2" style="background-image: url(assets/images/img_3.jpg);" href="{{ asset('assets/images/img_3.jpg') }}">
+                    <div class="details">
+                        <h4>Portfolio 2</h4>
+                        <span>Alored dono par</span>
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md-3">
+                <a class="portfolio-item fancybox" data-fancybox="gallery2" style="background-image: url(assets/images/img_4.jpg);" href="{{ asset('assets/images/img_4.jpg') }}">
+                    <div class="details">
+                        <h4>Portfolio 2</h4>
+                        <span>Alored dono par</span>
+                    </div>
+                </a>
+            </div>
+{{--             <div class="col-md-3">
+                <a class="portfolio-item" style="background-image: url(assets/img/portfolio-3.jpg);" href="">
+                    <div class="details">
+                        <h4>Portfolio 3</h4>
+                        <span>Alored dono par</span>
+                    </div>
+                </a>
+            </div>
+ --}}
+{{--             <div class="col-md-3">
+                <a class="portfolio-item" style="background-image: url(assets/img/portfolio-4.jpg);" href="">
+                    <div class="details">
+                        <h4>Portfolio 4</h4>
+                        <span>Alored dono par</span>
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md-3">
+                <a class="portfolio-item" style="background-image: url(assets/img/portfolio-5.jpg);" href="">
+                    <div class="details">
+                        <h4>Portfolio 5</h4>
+                        <span>Alored dono par</span>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-6 mb-4 mb-lg-0 col-lg-4" data-aos="fade-up" data-aos-delay="100">
+            </div>
+
+            <div class="col-md-3">
+                <a class="portfolio-item" style="background-image: url(assets/img/portfolio-6.jpg);" href="">
+                    <div class="details">
+                        <h4>Portfolio 6</h4>
+                        <span>Alored dono par</span>
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md-3">
+                <a class="portfolio-item" style="background-image: url(assets/img/portfolio-7.jpg);" href="">
+                    <div class="details">
+                        <h4>Portfolio 7</h4>
+                        <span>Alored dono par</span>
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md-3">
+                <a class="portfolio-item" style="background-image: url(assets/img/portfolio-8.jpg);" href="">
+                    <div class="details">
+                        <h4>Portfolio 8</h4>
+                        <span>Alored dono par</span>
+                    </div>
+                </a>
+            </div> --}}
+
+        </div>
+    </div>
 </section>
